2bf5c92378c5f24555fa8f6242ee49613961a2b8
CaseyNelson <caseynelson314@gmail.com>
2024-08-08 05:10:47 +0900

    抽象木を構築する関数を追加

--- minc/main.c
+++ minc/main.c
@@ -5,6 +5,35 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+///////////////////////////////////////////////////////////////////////////
+//
+//    トークナイザ
+//
+
 typedef enum
 {
     TK_RESERVED, // 記号
@@ -23,32 +52,6 @@ struct Token
 };
 
 Token *token;
-
-void error(char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
-
-// 入力プログラム
-char *user_input;
-
-void error_at(char* loc, char* fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-
-    int pos = loc - user_input;
-    fprintf(stderr, "%s\n", user_input);
-    fprintf(stderr, "%*s", pos, " ");
-    fprintf(stderr, "^ ");
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
 
 // トークンを読み、期待した記号であるとき次へ進め真を返す
 bool consume(char op)
@@ -82,7 +85,6 @@ bool at_eof()
 {
     return token->kind == TK_EOF;
 }
-
 
 // トークンを作成し、cur につなげる
 Token *new_token(TokenKind kind, Token *cur, char *str)
@@ -126,12 +128,100 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズ出来ません");
+        error_at(p, "トークナイズ出来ません");
     }
 
     new_token(TK_EOF, cur, p);
 
     return head.next;
+}
+
+///////////////////////////////////////////////////////////////////////////
+//
+//    パーサー
+//
+
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+
+Node *expr();
+
+// primary = num | "(" expr ")"
+Node *primary()
+{
+    if (consume('('))
+    {
+        Node *node = expr();
+        expect(')');
+    }
+
+    return new_node_num(expect_number());
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul()
+{
+    Node *node = primary();
+
+    for (;;)
+    {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+// expr= mul ("+" mul | "-" mul)*
+Node *expr()
+{
+    Node *node = mul();
+
+    for (;;)
+    {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -143,28 +233,29 @@ int main(int argc, char *argv[])
     }
 
     user_input = argv[1];
-
-    // トークナイズ
     token = tokenize(argv[1]);
+    
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", expect_number());
+    // printf("  mov rax, %d\n", expect_number());
 
     while (!at_eof())
     {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
+        // if (consume('+'))
+        // {
+        //     printf("  add rax, %d\n", expect_number());
+        //     continue;
+        // }
 
-        if (consume('-')) {
-            printf("  sub rax, %d\n", expect_number());
-            continue;
-        }
-        
-        error("予期しないエラー");
+        // if (consume('-'))
+        // {
+        //     printf("  sub rax, %d\n", expect_number());
+        //     continue;
+        // }
+
+        // error("予期しないエラー");
     }
 
     printf("  ret\n");
